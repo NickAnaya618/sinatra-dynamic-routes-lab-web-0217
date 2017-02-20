ada6c93a71883be5fbf10c1e9ed209efa7ca2c11
NickAnaya618 <anaya.nicholas@gmail.com>
2017-02-20 14:38:20 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,41 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/reversename/:name' do
+    @name = params[:name]
+    "#{@name.reverse}"
+  end
+
+  get '/square/:number' do
+    @number = params[:number]
+    "#{@number.to_i * @number.to_i}"
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number]
+    @phrase = params[:phrase]
+
+    @integer = @number.to_i
+
+    final_string = ""
+    @integer.times do
+      final_string += "#{@phrase}\n"
+    end
+
+    final_string
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:number1]
+    @number2 = params[:number2]
+
+    "#{@number1.to_i.send(@operation, @number2.to_i)}"
+  end
+
 end
